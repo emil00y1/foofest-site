893ac3ecaba8bb32e27678299f92d465c1df3be3
Emil Kristensen <emil00y1@stud.kea.dk>
2023-12-04 15:08:51 +0100

    foofest shad

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Yo</Button>
+      <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input type="email" placeholder="Email" />
+        <Button type="submit">Subscribe</Button>
+      </div>
+    </div>
+  );
+}
