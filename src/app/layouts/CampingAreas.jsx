63f111a87c9202d213b60c9abc951619a8190d51
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-12-04 15:42:59 +0100

    .

---
+++ src/app/layouts/CampingAreas.jsx
@@ -0,0 +1,33 @@
+"use client";
+import { React, useEffect, useState } from "react";
+import RadioTile from "@/components/RadioTile";
+
+async function fetchFunc() {
+  const res = await fetch("http://localhost:8080/available-spots");
+  const data = await res.json();
+  return data;
+}
+
+function CampingAreas() {
+  const [campingArea, setCampingArea] = useState([]);
+
+  useEffect(() => {
+    const x = async () => {
+      const data = await fetchFunc();
+      console.log(data);
+      setCampingArea(data);
+    };
+    x();
+    x;
+  }, []);
+
+  return (
+    <legend className="flex" defaultValue="private" aria-label="Visibility Level">
+      {campingArea.map((data) => (
+        <RadioTile key={data.area} data="data"></RadioTile>
+      ))}
+    </legend>
+  );
+}
+
+export default CampingAreas;
