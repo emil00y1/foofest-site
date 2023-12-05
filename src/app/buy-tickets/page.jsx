21f902cc88de4f4abdf779f05dd78df9535ffd80
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-12-05 11:40:37 +0100

    .

--- src/app/buy-tickets/page.jsx
+++ src/app/buy-tickets/page.jsx
@@ -1,9 +1,17 @@
 import CampingAreas from "../layouts/CampingAreas";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 function buyTickets() {
   return (
     <>
       <main>
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Input type="email" placeholder="Email" />
+          <Button type="submit" className="hover:bg-yellow-300">
+            Subscribe
+          </Button>
+        </div>
         <CampingAreas></CampingAreas>
       </main>
     </>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,26 @@
 "use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div>
-      <Button className="hover:bg-yellow-300">Yo</Button>
-      <div className="flex w-full max-w-sm items-center space-x-2">
-        <Input type="email" placeholder="Email" />
-        <Button type="submit" className="hover:bg-yellow-300">
-          Subscribe
-        </Button>
-      </div>
+    <main>
+      <h1>MUSIC FESTIVAL</h1>
+      <p>7 DAYS, 24/7</p>
+      <Button className="hover:bg-yellow-300">TICKETS</Button>
+      <Button variant="outline" className="hover:bg-yellow-300">
+        PROGRAM
+      </Button>
+
+      <h2>ABOUT FOOFEST</h2>
+      <p>
+        Our week-long 24/7 festival brings together a diverse lineup of artists for an unforgettable experience. Camp under the stars, enjoy top-notch facilities, and immerse yourself in the rhythm of
+        the music. Join us for a down-to-earth celebration at this year’s Foofest.
+      </p>
+
       <Footer />
-    </div>
+    </main>
   );
 }
