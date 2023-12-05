821722dd97e40b82b7b97765deabbc6765c7639a
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-12-05 15:39:18 +0100

    forside done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { caesar } from "./fonts";
 import Marquee from "react-fast-marquee";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -22,12 +23,16 @@ export default function Home() {
       <main className="m-6">
         <h1 className="text-4xl font-black text-center">MUSIC FESTIVAL</h1>
         <p className="text-3xl font-black text-center">7 DAYS, 24/7</p>
-        <Button className="hover:bg-yellow-300 py-2 w-52 block m-auto">TICKETS</Button>
-        <Button variant="outline" className="hover:bg-yellow-300 border-yellowaccent text-yellowaccent py-2 w-52 block m-auto">
-          PROGRAM
-        </Button>
-        <article className="flex flex-col gap-3">
-          <h2>ABOUT FOOFEST</h2>
+        <div className="flex flex-col gap-4 my-10">
+          <Button className="hover:bg-yellow-300 py-2 w-52 block m-auto">
+            <Link href="/buy-tickets">TICKETS</Link>
+          </Button>
+          <Button variant="outline" className="hover:bg-yellow-300 border-yellowaccent text-yellowaccent py-2 w-52 block m-auto">
+            <Link href="/lineup">PROGRAM</Link>
+          </Button>
+        </div>
+        <article className="flex flex-col gap-3 pb-4">
+          <h2 className="font-bold">ABOUT FOOFEST</h2>
           <p>Our week-long 24/7 festival brings together a diverse lineup of artists for an unforgettable experience.</p>
           <p>Camp under the stars, enjoy top-notch facilities, and immerse yourself in the rhythm of the music.</p>
           <p>Join us for a down-to-earth celebration at this year’s Foofest.</p>
