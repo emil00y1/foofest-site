ba95a9aa22b699ff3f54c06aca15dff3f2a8df63
Emil Kristensen <emil00y1@stud.kea.dk>
2023-12-05 11:39:58 +0100

    tailwind config

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,28 @@
+"use client";
 import Image from "next/image";
+import React from "react";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
 
 function Header() {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
   return (
-    <header className="flex flex-row p-6">
-      <Image src="/logo.svg" width="70" height="70"></Image>
-      <nav></nav>
+    <header className="flex flex-row align-between p-6">
+      <Image src="/logo.svg" alt="logo" width="70" height="70"></Image>
+      <div className="">
+        <button onClick={toggleDrawer}>Show</button>
+        <Drawer
+          open={isOpen}
+          onClose={toggleDrawer}
+          direction="right"
+          className=""
+        >
+          <div>Hello World</div>
+        </Drawer>
+      </div>
     </header>
   );
 }
