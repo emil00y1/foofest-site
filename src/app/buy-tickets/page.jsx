106a3ecfb960e67769156b148411554086b1aa7f
Emil Kristensen <emil00y1@stud.kea.dk>
2023-12-06 13:25:56 +0100

    radioswitch

--- src/app/buy-tickets/page.jsx
+++ src/app/buy-tickets/page.jsx
@@ -3,14 +3,19 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Headline from "@/components/Headline";
 import RadioSwitch from "@/components/RadioSwitch";
+import NumberInput from "@/components/NumberInput";
+import TicketOptions from "@/components/TicketOptions";
 
 function buyTickets() {
   return (
     <>
       <main>
         <Headline>Choose your tickets</Headline>
-        <div>Number of tickets</div>
-        <RadioSwitch />
+        <div>
+          Number of tickets <NumberInput />
+        </div>
+        <TicketOptions ticket="1" />
+        <TicketOptions ticket="2" />
       </main>
     </>
   );
