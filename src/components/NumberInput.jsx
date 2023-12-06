c8da2674c6858b4b35e9cd440c96268f5b709690
Emil Kristensen <emil00y1@stud.kea.dk>
2023-12-06 13:56:56 +0100

    numberinputs

--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -1,5 +1,12 @@
+import styles from "@/components/modules/NumberInput.module.css";
+
 function NumberInput() {
-  return <></>;
+  return (
+    <>
+      <label htmlFor="tickets"></label>
+      <input className="bg-background border-1" id="tickets" type="number" />
+    </>
+  );
 }
 
 export default NumberInput;
