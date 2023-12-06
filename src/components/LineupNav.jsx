c7b2c83d27c7ccee3c98bbf4a585a1d9dc2ff983
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-12-06 11:10:23 +0100

    øv stages

--- src/components/LineupNav.jsx
+++ src/components/LineupNav.jsx
@@ -1,18 +1,45 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
+
+async function fetchFunc() {
+  const res = await fetch("http://localhost:8080/schedule");
+  const data = await res.json();
+  return data;
+}
 
 function LineupNav() {
   const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-  const [day, setDay] = useState(true);
 
+  const [stages, setStages] = useState([]);
+  useEffect(() => {
+    const x = async () => {
+      const data = await fetchFunc();
+      console.log("data", data);
+      const stageKeys = Object.keys(data);
+
+      setStages((o) => o.concat(stageKeys));
+    };
+    x();
+    x;
+  }, []);
+  console.log("stages", stages);
   return (
     <nav>
-      {days.map((day) => (
-        <Button className="uppercase rounded-none" variant="outline">
-          {day}
-        </Button>
-      ))}
+      <div>
+        {days.map((day) => (
+          <Button className="uppercase rounded-none" variant="outline">
+            {day}
+          </Button>
+        ))}
+      </div>
+      <div>
+        {stages.map((stage) => (
+          <Button className="uppercase rounded-none" variant="outline">
+            {stage}
+          </Button>
+        ))}
+      </div>
     </nav>
   );
 }
