458168b42b0133cbafbc19de2482862ab0f2d7b1
Emil Kristensen <emil00y1@stud.kea.dk>
2023-12-08 14:51:23 +0100

    lineup functionality

--- src/app/lineup/page.jsx
+++ src/app/lineup/page.jsx
@@ -13,27 +13,54 @@ async function fetchFunc() {
 
 function Lineup() {
   const [bands, setBands] = useState([]);
+  const [stages, setStages] = useState([]);
+  const [selectedDay, setSelectedDay] = useState("sun");
+  const [selectedStage, setSelectedStage] = useState("Midgard");
 
   useEffect(() => {
     const x = async () => {
       const data = await fetchFunc();
-      console.log("artist", data.Midgard.fri);
-      const filteredData = data.Midgard.fri;
-      setBands((o) => o.concat(filteredData));
+      console.log("artist", data.Midgard.selectedDay);
+      // Memoize the filtered data function to ensure it's recalculated only when necessary
+      const getFilteredData = () => {
+        const filteredData = data[selectedStage][selectedDay];
+        return filteredData;
+      };
+
+      const bands = getFilteredData();
+      setBands(bands);
+    };
+    x();
+    x;
+  }, [selectedDay, selectedStage]);
+
+  useEffect(() => {
+    const x = async () => {
+      const data = await fetchFunc();
+      console.log("data", data);
+      const stageKeys = Object.keys(data);
+      setStages((o) => o.concat(stageKeys));
     };
     x();
     x;
   }, []);
 
   console.log("setBands", bands);
-
   return (
     <main>
       <Headline>Lineup</Headline>
-      <LineupNav></LineupNav>
+      <LineupNav
+        stages={stages}
+        setSelectedDay={setSelectedDay}
+        setSelectedStage={setSelectedStage}
+      ></LineupNav>
       <section className="p-3">
         {bands.map((band) => (
-          <ArtistCard act={band.act} start={band.start} end={band.end}></ArtistCard>
+          <ArtistCard
+            act={band.act}
+            start={band.start}
+            end={band.end}
+          ></ArtistCard>
         ))}
       </section>
     </main>
