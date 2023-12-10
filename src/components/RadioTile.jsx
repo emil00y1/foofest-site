f554c6cfac0361adf791f132b7de38c0685a0876
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-12-10 20:22:48 +0100

    :(

--- src/components/RadioTile.jsx
+++ src/components/RadioTile.jsx
@@ -5,18 +5,16 @@ function RadioTile({ area, availableSpots, spots, img }) {
     <div className="flex items-center">
       <label
         htmlFor={area}
-        className="cursor-pointer px-4 peer-checked:border-yellowaccent peer-focus-visible:ring peer-focus-visible:ring-orange-700 peer-focus-visible:ring-offset-2 w-full border-forground border-2 check:border-yellowaccent rounded-md flex items-center"
+        className={`cursor-pointer  p-5 peer-checked:border-yellowaccent peer-focus-visible:ring peer-focus-visible:ring-orange-700 peer-focus-visible:ring-offset-2 w-full border-forground border-2 check:border-yellowaccent rounded-md flex items-center bg-[url('/img/${area}.png')] bg-right bg-contain bg-no-repeat`}
       >
         <input type="radio" name="area" id={area} className="peer focus:outline-none accent-yellowaccent mr-6" />
         <span className="peer-checked:text-yellowaccent">
-          <span className="block text-xl">{area}</span>
+          <span className="block text-lg">{area}</span>
 
           <span className="block">
             {availableSpots}/{spots} spots left
           </span>
         </span>
-
-        <Image src={img} alt={area} width="214" height="107"></Image>
       </label>
     </div>
   );
