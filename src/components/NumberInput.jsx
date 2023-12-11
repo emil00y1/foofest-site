219df33e5bab54dbbcb07efbc9af242dbe3ebf5f
Emil Kristensen <emil00y1@stud.kea.dk>
2023-12-11 14:56:13 +0100

    dunno

--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -10,19 +10,37 @@ function NumberInput({ setAmount, amount, setStandAmount, setVipAmount }) {
             setAmount((old) => old - 1);
           }}
         >
-          <svg className="fill-foreground" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+          <svg
+            className="fill-foreground"
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+          >
             <path d="M200-440v-80h560v80H200Z" />
           </svg>
         </button>
         <label htmlFor="tickets"></label>
-        <input className="bg-background border-1 w-14 text-center" id="tickets" type="number" readOnly value={amount} />
+        <input
+          className="bg-background border-1 w-14 text-center"
+          id="tickets"
+          type="number"
+          readOnly
+          value={amount}
+        />
 
         <button
           onClick={() => {
             setAmount((old) => old + 1);
           }}
         >
-          <svg className="fill-foreground" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+          <svg
+            className="fill-foreground"
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+          >
             <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
           </svg>
         </button>
