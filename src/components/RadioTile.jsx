55039dcf687a51ee6ac57e294102c8d1747f95b9
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-12-11 09:12:51 +0100

    radio buttons driller

--- src/components/RadioTile.jsx
+++ src/components/RadioTile.jsx
@@ -5,7 +5,7 @@ function RadioTile({ area, availableSpots, spots, img }) {
     <div className="flex items-center">
       <label
         htmlFor={area}
-        className={`cursor-pointer  p-5 peer-checked:border-yellowaccent peer-focus-visible:ring peer-focus-visible:ring-orange-700 peer-focus-visible:ring-offset-2 w-full border-forground border-2 check:border-yellowaccent rounded-md flex items-center bg-[url('/img/${area}.png')] bg-right bg-contain bg-no-repeat`}
+        className={`cursor-pointer  p-5 peer-checked:border-yellowaccent peer-focus-visible:ring peer-focus-visible:ring-orange-700 peer-focus-visible:ring-offset-2 w-full border-forground border-2 check:border-yellowaccent rounded-md flex items-center bg-[url('/img/nilfheim.png')] bg-right bg-contain bg-no-repeat`}
       >
         <input type="radio" name="area" id={area} className="peer focus:outline-none accent-yellowaccent mr-6" />
         <span className="peer-checked:text-yellowaccent">
