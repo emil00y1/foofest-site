b561be68a856dea19981ff87d71f3bd7209050fa
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-12-11 09:48:20 +0100

    .

--- src/app/buy-tickets/page.jsx
+++ src/app/buy-tickets/page.jsx
@@ -1,16 +1,17 @@
 import CampingAreas from "@/components/CampingAreas";
 import CampingPreference from "@/components/CampingPreference";
 import Headline from "@/components/Headline";
+import PersonalInfo from "@/components/PersonalInfo";
 import TicketType from "@/components/TicketType";
 
 function buyTickets() {
   return (
     <>
       <main className="m-4">
-        {/*         <TicketType />
-         */}
-        <CampingAreas />
-        {/* <CampingPreference /> */}
+        {/* <TicketType /> */}
+        {/*   <CampingAreas /> */}
+        {/*  <CampingPreference /> */}
+        <PersonalInfo />
       </main>
     </>
   );
---
+++ src/components/PersonalInfo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Headline from "./Headline";
+import { Input } from "postcss";
+
+function PersonalInfo() {
+  return <Headline>Personal info</Headline>;
+}
+
+export default PersonalInfo;
