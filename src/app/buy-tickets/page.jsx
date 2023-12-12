1db43ae02bb0f793a292cd69fe58bc0e64233668
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-12-12 09:50:00 +0100

    .

--- src/app/buy-tickets/page.jsx
+++ src/app/buy-tickets/page.jsx
@@ -7,8 +7,35 @@ import PaymentInfo from "@/components/PaymentInfo";
 import PersonalInfo from "@/components/PersonalInfo";
 import TicketType from "@/components/TicketType";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 
 function BuyTickets() {
+  const form =
+    useForm <
+    z.infer <
+    typeof formSchema >>
+      {
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          username: "",
+        },
+      };
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   const [pageView, setPageView] = useState(1);
   const handleSubmit = (event) => {
     event.preventDefault();
