952c1b82cac542db05d4fe4dc35822a163b49c9f
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-12-12 16:22:05 +0100

    tailwind vil ikke hvad jeg vil

--- src/components/RadioTile.jsx
+++ src/components/RadioTile.jsx
@@ -13,9 +13,9 @@ function RadioTile({ area, availableSpots, spots, setChosenArea }) {
   return (
     <label
       htmlFor={area}
-      className={`cursor-pointer p-5 peer-checked:border-yellowaccent peer-focus-visible:ring peer-focus-visible:ring-orange-700 peer-focus-visible:ring-offset-2 w-full border-forground border-2 check:border-yellowaccent rounded-md flex items-center bg-[url('/img/nilfheim.png')] bg-right bg-contain bg-no-repeat`}
+      className={`radio-label cursor-pointer p-5 first:has(checked):border-yellowaccent w-full border-forground border-2 rounded-md flex items-center bg-[url('/img/nilfheim.png')] bg-right bg-contain bg-no-repeat`}
     >
-      <input type="radio" name="area" id={area} className="peer focus:outline-none accent-yellowaccent mr-6" onChange={radioChange} />
+      <input type="radio" name="area" id={area} className="cursor-pointer peer focus:outline-pink-600 accent-yellowaccent mr-6" onChange={radioChange} />
       <span className="peer-checked:text-yellowaccent">
         <span className="block text-lg">{area}</span>
 
