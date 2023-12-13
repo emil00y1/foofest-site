817ac3fffbfb111dbbf6c845b0e7b398db87df93
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-12-13 21:41:40 +0100

    mangler lidt på checkbox

--- src/components/RadioTile.jsx
+++ src/components/RadioTile.jsx
@@ -1,13 +1,6 @@
 import { useState } from "react";
 
-function RadioTile({
-  area,
-  availableSpots,
-  spots,
-  setChosenArea,
-  chosenArea,
-  setErrorMsg,
-}) {
+function RadioTile({ area, availableSpots, spots, setChosenArea, chosenArea, setErrorMsg }) {
   const [isAreaChecked, setIsAreaChecked] = useState(false);
 
   const radioChange = (event) => {
@@ -20,18 +13,11 @@ function RadioTile({
       htmlFor={area}
       className={`radio-label cursor-pointer p-5 first:has(checked):border-yellowaccent w-full border-forground border-2 rounded-md flex items-center bg-[url('/img/nilfheim.png')] bg-right bg-contain bg-no-repeat`}
     >
-      <input
-        type="radio"
-        name="area"
-        defaultChecked={chosenArea === area}
-        id={area}
-        className="cursor-pointer peer focus:outline-pink-600 accent-yellowaccent mr-6"
-        onChange={radioChange}
-      />
-      <span className="peer-checked:text-yellowaccent">
-        <span className="block text-lg">{area}</span>
+      <input type="radio" name="area" defaultChecked={chosenArea === area} id={area} className="cursor-pointer peer focus:outline-pink-600 accent-yellowaccent mr-6" onChange={radioChange} />
+      <span className="group/info peer-checked:text-yellowaccent group-hover:text-yellowaccent">
+        <span className="block text-lg info">{area}</span>
 
-        <span className="block">
+        <span className="block info">
           {availableSpots}/{spots} spots left
         </span>
       </span>
